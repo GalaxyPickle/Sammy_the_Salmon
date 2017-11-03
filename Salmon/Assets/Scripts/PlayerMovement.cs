52aa40db251c4028b3053d5e35bf87327190c6ec
Alex <alex@clancylang.com>
2017-11-02 20:53:51 -0700

    basic movement and testing environment made

---
+++ Salmon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int MOVESPEED;
+	public int Y_LEVEL;
+	public int Z_LEVEL;
+	public GameObject rail;
+
+	// Use this for initialization
+	void Start () {
+		// hold a copy of the mother rail object for reference
+		rail = GameObject.Find ("Rail");
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * MOVESPEED;
+		transform.Translate(x, 0, 0);
+
+		// lock player movement onto forward and sideways axes
+		Vector3 pos = transform.position;
+		pos.y = Y_LEVEL;
+		pos.z = rail.transform.position.z + Z_LEVEL;
+		transform.position = pos;
+
+		// Lock player rotation to nothing for right now
+		Quaternion rot = transform.rotation;
+		rot.x = 0;
+		rot.y = 0;
+		rot.z = 0;
+		transform.rotation = rot;
+	}
+}
---
+++ Salmon/Assets/Scripts/RailMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMovement : MonoBehaviour {
+
+	public int railspeed = 10;
+	public bool move = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			move = !move;
+		}
+		if (move) {
+			transform.Translate (Vector3.forward * railspeed * Time.deltaTime);
+		}
+	}
+}
